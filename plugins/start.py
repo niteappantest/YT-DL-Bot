6ad99785d65a84e12bba4ce871b39b138626a457
niteappantest <82845096+niteappantest@users.noreply.github.com>
2021-05-22 13:50:21 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,6 +9,6 @@ async def start(client, message):
         [InlineKeyboardButton(
             "Report Bugs 😊", url="https://t.me/WH173_5P1D3R")]
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\nIam YT Downloader Bot! [📥](https://t.me/WH173_5P1D3R)\nI'M YT DL Bot 🤖 By @WH173_5P1D3R\n/help for More info"
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\nIam YT Downloader Bot! [📥]\nI'M Downloader Bot 🤖 By @WH173_5P1D3R\n/help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
